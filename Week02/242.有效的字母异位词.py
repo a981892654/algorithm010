54dafcde54ba24a85d5df56724c3820a6f4d75e0
GeekUniversity <981892654@qq.com>
2020-06-21 23:45:11 +0800

    week01 assignment

---
+++ Week02/242.有效的字母异位词.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=242 lang=python
+#
+# [242] 有效的字母异位词
+#
+
+# @lc code=start
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t): return False
+        se = set(s)
+        if se == set(t):
+            for i in se:
+                # 直接比较字符元素个数比较字符的个数
+                if s.count(i) != t.count(i):return False
+            return True
+        else:
+            return False
+
+
+# @lc code=end
+
